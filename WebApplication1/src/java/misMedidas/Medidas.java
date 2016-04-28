f7da109abe796a1a7cc241506e7c296071c3fd1f
lyseth <yohanacorba11@hotmail.com>
2016-04-28 15:26:33 -0500

    Subo pagina de Mis medidas

---
+++ WebApplication1/src/java/misMedidas/Medidas.java
@@ -0,0 +1,36 @@
+package misMedidas;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.event.ActionEvent;
+
+/**
+ *
+ * @author DT8
+ */
+@ManagedBean
+public class Medidas {
+    
+    private String peso;
+    private String nivelColesterol;
+
+    public String getPeso() {
+        return "peso.xhtml";
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public String getNivelColesterol() {
+        return "nivelColesterol.xtml";
+    }
+
+    public void setNivelColesterol(String nivelColesterol) {
+        this.nivelColesterol = nivelColesterol;
+    }
+    
+    
+    
+    
+
+}
