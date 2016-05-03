2ebcbfec1403278289d3deb3954d0b3d75a8e77a
lyseth <yohanacorba11@hotmail.com>
2016-05-03 13:02:30 -0500

    Corrigo subida de la pagina registro con algunas modificaciones de la pagina.

---
+++ HospitalPanamericano/web/Registro/src/java/SelectorView.java
@@ -0,0 +1,90 @@
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+
+public class SelectorView {
+     
+    private String text1;   
+    private String text2;
+    private String text3;
+    private int text4;
+    private int text5;
+    private String text6;
+    private int text7;
+    private String text8;
+    private String text9;
+    
+ 
+    public String getText1() {
+        return text1;
+    }
+ 
+    public void setText1(String text1) {
+        this.text1 = text1;
+    }
+ 
+    public String getText2() {
+        return text2;
+    }
+ 
+    public void setText2(String text2) {
+        this.text2 = text2;
+    }
+ 
+    public String getText3() {
+        return text3;
+    }
+ 
+    public void setText3(String text3) {
+        this.text3 = text3;
+    }
+
+    public int getText4() {
+        return text4;
+    }
+
+    public void setText4(int text4) {
+        this.text4 = text4;
+    }
+
+    public int getText5() {
+        return text5;
+    }
+
+    public void setText5(int text5) {
+        this.text5 = text5;
+    }
+
+    public String getText6() {
+        return text6;
+    }
+
+    public void setText6(String text6) {
+        this.text6 = text6;
+    }
+
+    public int getText7() {
+        return text7;
+    }
+
+    public void setText7(int text7) {
+        this.text7 = text7;
+    }
+
+    public String getText8() {
+        return text8;
+    }
+
+    public void setText8(String text8) {
+        this.text8 = text8;
+    }
+
+    public String getText9() {
+        return text9;
+    }
+
+    public void setText9(String text9) {
+        this.text9 = text9;
+    }
+}
+    
---
+++ HospitalPanamericano/web/Registro/src/java/org/primefaces/showcase/view/input/password.java
@@ -0,0 +1,18 @@
+package org.primefaces.showcase.view.input;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class password {
+    
+    private String password5;
+
+ 
+   
+    public String getPassword5() {
+        return password5;
+    }
+
+    public void setPassword5(String password5) {
+        this.password5 = password5;
+    }
+}
