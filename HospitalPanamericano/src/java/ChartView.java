af8dd26eaafb76fc8f8ca597285c799db4b58c4c
lyseth <yohanacorba11@hotmail.com>
2016-05-03 14:13:21 -0500

    Correccion subida a github pagina mis medidas (nive coresterol) java class

--- HospitalPanamericano/src/java/ChartView.java
+++ HospitalPanamericano/src/java/ChartView.java
@@ -26,31 +26,31 @@ public class ChartView implements Serializable {
         dateModel = new LineChartModel();
         LineChartSeries series1 = new LineChartSeries();
         dateModel.setLegendPosition("ne");
-        series1.setLabel("Mi peso");
+        series1.setLabel("Nivel Coresterol");
  
         series1.set("2016-01-01", 51);
-        series1.set("2016-02-06", 60);
-        series1.set("2016-03-12", 65);
-        series1.set("2016-04-18", 57);
+        series1.set("2016-02-06", 25);
+        series1.set("2016-03-12", 30);
+        series1.set("2016-04-18", 20);
         series1.set("2016-05-24", 24);
        
  
         LineChartSeries series2 = new LineChartSeries();
         series2.setLabel("Recomendacion Medico");
  
-        series2.set("2016-01-01", 32);
-        series2.set("2016-02-06", 73);
-        series2.set("2016-03-12", 24);
-        series2.set("2016-04-18", 12);
-        series2.set("2016-05-24", 74);
+        series2.set("2016-01-01", 42);
+        series2.set("2016-02-06", 22);
+        series2.set("2016-03-12", 30);
+        series2.set("2016-04-18", 22);
+        series2.set("2016-05-24", 24);
         
  
         dateModel.addSeries(series1);
         dateModel.addSeries(series2);
          
-        dateModel.setTitle("Mi peso");
+        dateModel.setTitle(" Mi Nivel De Coresterol");
         
-        dateModel.getAxis(AxisType.Y).setLabel("Peso");
+        dateModel.getAxis(AxisType.Y).setLabel("Nivel Coresterol");
         DateAxis axis = new DateAxis("Mes");
         axis.setTickAngle(-9);
         axis.setMax("2016-05-01");
