fab6fee45453286f16b5407ddb87801698cd1af7
lyseth <yohanacorba11@hotmail.com>
2016-05-30 14:05:06 -0500

    Subo java class de  datos adicionales (aun faltan modificaciones)

---
+++ HospitalPanamericano/src/java/datos.java
@@ -0,0 +1,71 @@
+package DatosAdicionales;
+
+
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class datos {
+     
+    private String text1;   
+    private String text2;
+    private String text3;
+    private String text4;
+    private String text5;
+     /*private String[] selectedConsoles;
+    private String[] selectedConsoles1;
+    private String[] selectedConsoles2;
+    private String[] selectedConsoles3;
+    private String[] selectedConsoles4; */
+    
+    public String getText1() {
+        return text1;
+    }
+ 
+    public void setText1(String text1) {
+        this.text1 = text1;
+    }
+ 
+    public String getText2() {
+        return text2;
+    }
+ 
+    public void setText2(String text2) {
+        this.text2 = text2;
+    }
+ 
+    public String getText3() {
+        return text3;
+    }
+ 
+    public void setText3(String text3) {
+        this.text3 = text3;
+    }
+
+    public String getText4() {
+        return text4;
+    }
+
+    public void setText4(String text4) {
+        this.text4 = text4;
+    }
+
+    public String getText5() {
+        return text5;
+    }
+
+    public void setText5(String text5) {
+        this.text5 = text5;
+    }
+    
+    
+
+    
+
+    
+    }
+
+  
+    
+    
+
+
